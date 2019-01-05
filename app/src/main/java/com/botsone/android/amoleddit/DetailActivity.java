c6d2ff730c6bbc67bbcc7fb57a1af34a2b476eb2
bots <botsone@gmail.com>
2019-01-05 10:16:45 -0700

    added floating action menu and fixed dialog

--- app/src/main/java/com/botsone/android/amoleddit/DetailActivity.java
+++ app/src/main/java/com/botsone/android/amoleddit/DetailActivity.java
@@ -1,38 +1,40 @@
 package com.botsone.android.amoleddit;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.provider.MediaStore;
-import android.provider.SyncStateContract;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.facebook.binaryresource.BinaryResource;
-import com.facebook.binaryresource.FileBinaryResource;
-import com.facebook.cache.common.CacheKey;
-import com.facebook.drawee.view.DraweeView;
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.cache.DefaultCacheKeyFactory;
-import com.facebook.imagepipeline.core.ImagePipelineFactory;
-import com.facebook.imagepipeline.request.ImageRequest;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
 import java.io.IOException;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String TAG = DetailActivity.class.getName();
+    private static final String SAMPLE_CROPPED_IMAGE_NAME = "tempImage";
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    Uri uri3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,92 +45,154 @@ public class DetailActivity extends AppCompatActivity {
 
         final SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.detail_view);
 
+        int actionRequested = 0;
+
         Intent intent = getIntent();
 
         final String value = intent.getStringExtra("key");
+
+        final Uri parsedUri = Uri.parse(value);
+        String destinationFileName = SAMPLE_CROPPED_IMAGE_NAME + ".png";
+        final Uri destinationUri=Uri.fromFile(new File(getCacheDir(), destinationFileName));
 
         draweeView.setDrawingCacheEnabled(true);
         draweeView.buildDrawingCache();
         draweeView.setImageURI(value);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionsMenu fam = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
 
         final Context context = DetailActivity.this;
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        final WallpaperManager wm = WallpaperManager.getInstance(context);
 
-                final Bitmap bitmap = draweeView.getDrawingCache();
-                if (bitmap != null) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                    builder.setTitle("Set Wallpaper");
-                    builder.setItems(new CharSequence[]
-                                    {"Homescreen", "Lockscreen", "Both"},
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // The 'which' argument contains the index position
-                                    // of the selected item
-                                    switch (which) {
-                                        case 0:
-                                            WallpaperManager wm1 = WallpaperManager.getInstance(getApplicationContext());
-                                            try {
-                                                wm1.setBitmap(bitmap);
-                                            } catch (Exception ex) {
-                                                ex.printStackTrace();
-                                            }
-                                            Toast.makeText(context, R.string.wallpaperSet, Toast.LENGTH_SHORT).show();
-                                            break;
-                                        case 1:
-                                            WallpaperManager wm2 = WallpaperManager.getInstance(getApplicationContext());
-                                            try {
-                                                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-                                                    // Do something for N and above versions
-                                                    wm2.setBitmap(bitmap, null, true, WallpaperManager.FLAG_LOCK);
-                                                    Toast.makeText(context, R.string.wallpaperSet, Toast.LENGTH_SHORT).show();
-                                                } else{
-                                                    // do something for phones running an SDK before N
-                                                    Toast.makeText(context, R.string.sorryMessage, Toast.LENGTH_SHORT).show();
-                                                }
-                                            }catch (Exception ex) {
-                                                ex.printStackTrace();
-                                            }
-                                            break;
-                                        case 2:
-                                            WallpaperManager wm3 = WallpaperManager.getInstance(getApplicationContext());
-                                            try {
-                                                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-                                                    // Do something for N and above versions
-                                                    wm3.setBitmap(bitmap);
-                                                    wm3.setBitmap(bitmap, null, true, WallpaperManager.FLAG_LOCK);
-                                                    Toast.makeText(context, R.string.wallpaperSet, Toast.LENGTH_SHORT).show();
-                                                } else{
-                                                    // do something for phones running an SDK before N
-                                                    Toast.makeText(context, R.string.sorryMessage, Toast.LENGTH_SHORT).show();
-                                                }
-                                            }catch (Exception ex) {
-                                                ex.printStackTrace();
-                                            }
-                                            break;
+        final com.getbase.floatingactionbutton.FloatingActionButton actionA = (com.getbase.floatingactionbutton.FloatingActionButton) findViewById(R.id.action_a);
 
-                                    }
-                                }
-                            });
-                    builder.create().show();
+        actionA.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View view) {
 
-                }
+                                           UCrop.of(parsedUri, destinationUri)
+                                                   .start(DetailActivity.this);
 
+                                           
 
-            }
-        });
+//                                           try{
+//                                               Bitmap bitmap = draweeView.getDrawingCache();
+//                                               wm.setBitmap(bitmap);
+//                                           }
+//                                           catch(Exception ex)
+//                                           {
+//                                               ex.printStackTrace();
+//                                           }
+                                       }
+                                   });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public Uri getCurrentArticleUri() {
-        Intent intent = getIntent();
-        final String value = intent.getStringExtra("key");
-        Uri uri = Uri.parse(value);
-        return uri;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            Uri resultUri = UCrop.getOutput(data);
+            
+            showDialog(resultUri);
+            
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+        }
+    }
+
+    private void showDialog(Uri resultUri) {
+        DialogActivity cdd=new DialogActivity(this, resultUri);
+        cdd.show();
+    }
+
+    public class DialogActivity extends Dialog implements
+            android.view.View.OnClickListener {
+
+        public Activity c;
+        private Uri resultUri;
+        public Dialog d;
+        public Button regularWallpaper, lockscreenWallpaper, bothWallpapers;
+
+        public DialogActivity(Activity a, Uri resultUri) {
+            super(a);
+            // TODO Auto-generated constructor stub
+            this.c = a;
+            this.resultUri = resultUri;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.dialog);
+            regularWallpaper = (Button) findViewById(R.id.btn_wallpaper);
+            lockscreenWallpaper = (Button) findViewById(R.id.btn_lockscreen_wallpaper);
+            bothWallpapers = (Button) findViewById(R.id.btn_both_wallpapers);
+            regularWallpaper.setOnClickListener(this);
+            lockscreenWallpaper.setOnClickListener(this);
+            bothWallpapers.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_wallpaper:
+                    WallpaperManager wm = WallpaperManager.getInstance(DetailActivity.this);
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(DetailActivity.this.getContentResolver(), resultUri);
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            wm.setBitmap(bitmap, null, true, WallpaperManager.FLAG_SYSTEM);
+                            Toast.makeText(DetailActivity.this, "Wallpaper Set.", Toast.LENGTH_SHORT).show();
+                            c.finish();
+                        } else {
+                            Toast.makeText(DetailActivity.this, "Not available on this version of Android.", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    c.finish();
+                    break;
+                case R.id.btn_lockscreen_wallpaper:
+                    WallpaperManager wm2 = WallpaperManager.getInstance(DetailActivity.this);
+
+                    
+                    try {
+                        Bitmap bitmap2 = MediaStore.Images.Media.getBitmap(DetailActivity.this.getContentResolver(), resultUri);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            wm2.setBitmap(bitmap2, null, true, WallpaperManager.FLAG_LOCK);
+                            Toast.makeText(DetailActivity.this, "Wallpaper Set.", Toast.LENGTH_SHORT).show();
+                            c.finish();
+                        } else {
+                            Toast.makeText(DetailActivity.this, "Not available on this version of Android.", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+
+
+                    break;
+                case R.id.btn_both_wallpapers:
+                    WallpaperManager wm3 = WallpaperManager.getInstance(DetailActivity.this);
+
+                    try {
+                        Bitmap bitmap3 = MediaStore.Images.Media.getBitmap(DetailActivity.this.getContentResolver(), resultUri);
+                        wm3.setBitmap(bitmap3);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    break;
+            }
+            dismiss();
+        }
     }
 
 }
